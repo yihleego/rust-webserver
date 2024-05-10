ea1427e96021dd5ebba749995b10fc8590291445
Leego <i@leego.io>
2024-05-10 20:04:14 +0800

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::{fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}};
+use std::{fs, thread};
+use std::io::{BufReader, prelude::*};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
+        pool.execute(|| { handle_connection(stream.unwrap()); });
     }
 }
 
@@ -19,11 +25,15 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {:#?}", http_request.clone());
 
     let request_line = http_request.get(0).unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
+    let (status_line, filename) =
+        if request_line.starts_with("GET / ") {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if request_line.starts_with("GET /sleep") {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
     let contents = fs::read_to_string(filename).unwrap();
 
